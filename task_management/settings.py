0fef401feeb2b65147fabc92a2429584747a3313
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-04-05 22:05:53 +0000

    test

--- task_management/settings.py
+++ task_management/settings.py
@@ -30,9 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(xwmmhvrc(1#)wc4n3j11i68x$21v@f27jf2e-w=6m53%%#^3*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False 
+ALLOWED_HOSTS = ['hopemolebaloa.pythonanywhere.com']
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -136,7 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/hopemolebaloa/task-management-api/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
