1d8c81af62c477840df5f1821aa27e4898af6006
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-04-05 21:33:14 +0000

    tryyy

--- task_management/urls.py
+++ task_management/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/', include('api.urls')),
+    # Add this line to redirect from root to /api/
+    path('', RedirectView.as_view(url='/api/', permanent=False)),
 ]
